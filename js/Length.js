12c0d37e3e688716c1b575393f39f1dc71ef159c
Sanjithj <sanjithj591997@gmail.com>
2022-07-31 08:50:00 +0530

    Added: Length

---
+++ js/Length.js
@@ -0,0 +1,8 @@
+function add( a, b) {
+    return a+b;
+}
+function findLength(str){
+    return str.length;
+}
+console.log(add(11,13))
+console.log("The length of the string is: "+findLength("Sanjith"));
